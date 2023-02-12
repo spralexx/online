326d9cc65d9d9e752a35b7a9bab3ee9efd156a44
Szymon Kłos <szymon.klos@collabora.com>
2023-02-12 19:14:02 +0100

    notebookbar: simplify presentation button
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ia23703365a352a09ca8a8f255d9937b9fb99b433

--- browser/src/control/Control.NotebookbarImpress.js
+++ browser/src/control/Control.NotebookbarImpress.js
@@ -329,9 +329,9 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 	getViewTab: function() {
 		var content = [
 			{
-				'type': 'bigtoolitem',
+				'type': 'bigcustomtoolitem',
 				'text': _('Presentation'),
-				'command': '.uno:Presentation'
+				'command': 'presentation'
 			},
 			{
 				'id': 'fullscreen',
@@ -802,9 +802,9 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 				'vertical': 'true'
 			},
 			{
-				'type': 'bigtoolitem',
+				'type': 'bigcustomtoolitem',
 				'text': _('Presentation'),
-				'command': '.uno:Presentation'
+				'command': 'presentation'
 			},
 			{
 				'type': 'bigtoolitem',
